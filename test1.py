ebc08ddba7bb8668227f0b8f64831afb41a6c815
realseazonli <real.seazonli@gmail.com>
2021-02-17 21:23:03 +0800

    max_num in a_list

--- test1.py
+++ test1.py
@@ -1,16 +1,8 @@
 def find_max(a_list):
-    i = 0
-    result = 0
-    if a_list == '':
-        print(result)
-    else :
-        for i in range(len(a_list) - 1):
-            if a_list[i] >= a_list[i + 1]:
-                result = a_list[i]
-            else :
-                result = a_list[i + 1]
-            i += 1
-        return result
-
-
-print(find_max([1, 3, 2, 5, 18, 11, 9, 29, 6]))
+    if not a_list:
+        return 0
+    max_num = a_list[0]
+    for num in a_list:
+        if num > max_num:
+            max_num = num
+    return max_num
